7202ade77592e05b3d8ba129a2369bfcdb4837fb
SandeepKumar <sandeepkusolanki@gamil.com>
2025-03-27 18:20:53 +0530

    list

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react'
-
+let arr = [
+  "subrato", "krishna", "sandeep"
+]
 const App = () => {
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <ol>
+        {arr.map((value, index) => (
+          <li key={index}>{value}</li>
+        ))}
+      </ol>
     </div>
   )
 }
